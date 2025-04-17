cb7af72a69412f66c8a0fb4905a077ddc4cd839a
Juan Pablo Jaramillo G. <juanpjaramillo31@gmail.com>
2025-04-17 18:29:30 -0500

    feat se completa el banner de social media

--- src/components/socialMedia.jsx
+++ src/components/socialMedia.jsx
@@ -3,7 +3,21 @@ import "./../assets/estilos.css"
 export default function SocialMedia() {
   return (
     <div id="socialMedia">
-        <img className="imgSocial" src={"https://www.svgrepo.com/show/473495/youtube.svg"} alt="" />
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="linkSocial">
+           <p> <img src={"./../../public/img/facebook.svg"} alt="" className="imgSocial" /> </p>
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="linkSocial">
+           <p> <img src={"./../../public/img/twitter.svg"} alt="" className="imgSocial" /> </p>
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="linkSocial">
+           <p> <img src={"./../../public/img/instagram.svg"} alt="" className="imgSocial" /> </p>
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="linkSocial">
+           <p> <img src={"./../../public/img/pinterest.svg"} alt="" className="imgSocial" /> </p>
+        </a>
+        <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer" className="linkSocial">
+           <p> <img src={"./../../public/img/youtube.svg"} alt="" className="imgSocial" /> </p>
+        </a>
     </div>
   )
 }
